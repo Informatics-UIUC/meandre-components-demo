c834cb23dfdeb9cbf0f81ee1fa0ee2b4b092d154
Lily Dong <lilydong@illinois.edu>
2008-10-01 18:15:32 +0000

    add code for dealing with empty line

--- src/org/meandre/demo/components/io/StringLinePusher.java
+++ src/org/meandre/demo/components/io/StringLinePusher.java
@@ -87,6 +87,9 @@ public class StringLinePusher implements ExecutableComponent {
         cc.pushDataComponentToOutput(DATA_OUTPUT, new StreamInitiator());
         try {
             while((line = br.readLine())!= null) {
+                line = line.trim();
+                if(line.length() == 0)
+                    continue;
                 //System.out.println(line);
                 cc.pushDataComponentToOutput(DATA_OUTPUT, line);
             }
