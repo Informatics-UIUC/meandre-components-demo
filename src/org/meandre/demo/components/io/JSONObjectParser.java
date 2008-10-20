3a4e7084983164449dea7fa695d33ca1603d77bb
Lily Dong <lilydong@illinois.edu>
2008-10-20 17:36:55 +0000

    components for json

---
+++ src/org/meandre/demo/components/io/JSONObjectParser.java
@@ -0,0 +1,99 @@
+/**
+ * University of Illinois/NCSA
+ * Open Source License
+ *
+ * Copyright (c) 2008, Board of Trustees-University of Illinois.
+ * All rights reserved.
+ *
+ * Developed by:
+ *
+ * Automated Learning Group
+ * National Center for Supercomputing Applications
+ * http://www.seasr.org
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal with the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimers.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimers in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the names of Automated Learning Group, The National Center for
+ *    Supercomputing Applications, or University of Illinois, nor the names of
+ *    its contributors may be used to endorse or promote products derived from
+ *    this Software without specific prior written permission.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * WITH THE SOFTWARE.
+ */
+
+package org.meandre.demo.components.io;
+
+import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
+
+import org.meandre.annotations.Component;
+import org.meandre.annotations.ComponentInput;
+import org.meandre.annotations.ComponentOutput;
+
+import org.meandre.core.ComponentContext;
+import org.meandre.core.ComponentContextException;
+import org.meandre.core.ComponentContextProperties;
+import org.meandre.core.ComponentExecutionException;
+import org.meandre.core.ExecutableComponent;
+
+@Component(creator="Lily Dong",
+           description="Parse input text and convert it into JSONObject.",
+           name="JSONObjectParser",
+           tags="json, converter")
+
+public class JSONObjectParser implements ExecutableComponent {
+    @ComponentInput(description="Text to be parsed.",
+                    name= "inpuText")
+    public final static String DATA_INPUT = "inpuText";
+
+    @ComponentOutput(description="JSONObject converted from input text.",
+                     name="outputObject")             
+    public final static String DATA_OUTPUT = "outputObject";
+    
+    /** When ready for execution.
+    *
+    * @param cc The component context
+    * @throws ComponentExecutionException An exeception occurred during execution
+    * @throws ComponentContextException Illigal access to context
+    */
+    public void execute(ComponentContext cc) throws ComponentExecutionException,
+        ComponentContextException {
+        String inpuText = (String)cc.getDataComponentFromInput(DATA_INPUT);
+        JSONObject jo = (JSONObject)JSONSerializer.toJSON(inpuText);
+        
+        //System.out.println(jo.toString());
+        
+        cc.pushDataComponentToOutput(DATA_OUTPUT, jo);
+    }
+    
+    /**
+     * Call at the end of an execution flow.
+     */
+    public void initialize(ComponentContextProperties ccp) {
+    }
+    
+    /**
+     * Called when a flow is started.
+     */
+    public void dispose(ComponentContextProperties ccp) {
+    }
+}
