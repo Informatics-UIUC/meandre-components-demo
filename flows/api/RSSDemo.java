65640f452f627beeacfc82397d198c989a7ad1df
Peter Groves <pgroves@illinois.edu>
2008-12-19 05:15:05 +0000

    meandre-devkit-1.4.0.jar
     * this jar has FlowBuilderAPI in it. the package has changed to org.meandre.tools.flows.FlowBuilderAPI
    
    flows/api/*
    
     * changed package name of FlowBuilderAPI import statements

--- flows/api/RSSDemo.java
+++ flows/api/RSSDemo.java
@@ -1,7 +1,7 @@
 package api;
 
-import org.meandre.tools.components.FlowBuilderAPI;
-import org.meandre.tools.components.FlowBuilderAPI.WorkingFlow;
+import org.meandre.tools.flows.FlowBuilderAPI;
+import org.meandre.tools.flows.FlowBuilderAPI.WorkingFlow;
 
 public class RSSDemo {
 	public static void main(String[] args) {
--- flows/api/TagCloudDemo.java
+++ flows/api/TagCloudDemo.java
@@ -1,7 +1,7 @@
 package api;
 
-import org.meandre.tools.components.FlowBuilderAPI;
-import org.meandre.tools.components.FlowBuilderAPI.WorkingFlow;
+import org.meandre.tools.flows.FlowBuilderAPI;
+import org.meandre.tools.flows.FlowBuilderAPI.WorkingFlow;
 
 public class TagCloudDemo {
 	public static void main(String[] args) {
