bf0285a65dd86fb66e24e2bb651e9ce55bb1a07c
Lily Dong <lilydong@illinois.edu>
2009-02-13 17:05:44 +0000

    add more comment

--- src/org/meandre/components/io/evernote/EvernoteWriter.java
+++ src/org/meandre/components/io/evernote/EvernoteWriter.java
@@ -53,7 +53,7 @@ import org.meandre.core.ComponentExecutionException;
 import org.meandre.core.ExecutableComponent;
 
 import com.facebook.thrift.protocol.TBinaryProtocol;
-import com.facebook.thrift.transport.THttpClient; 
+import com.facebook.thrift.transport.THttpClient;
 
 import com.evernote.edam.notestore.NoteStore;
 import com.evernote.edam.type.User;
@@ -63,19 +63,23 @@ import com.evernote.edam.userstore.AuthenticationResult;
 import com.evernote.edam.userstore.UserStore;
 
 @Component(creator="Lily Dong",
-           description="Writes a string containing EvernoteMarkupLanguage " +
-           "(ENML) to an account on Evernote.com. The contents must strictly" + 
-           "comply with Evernote Markup Language(ENML) " ,
+           description="This component demonstrates how to implement " +
+           "a interface to write a string containing EvernoteMarkupLanguage " +
+           "(ENML) to an account on Evernote.com. It should be noted that " +
+           "the string must strictly comply with Evernote Markup Language(ENML)." +
+           "For checking the notes on your account, " +
+           "going to https://lb.evernote.com/Login.action " +
+           "and submitting your usename and password.",
            name="EvernoteWriter",
            tags="evernote, note, notebook",
            baseURL="meandre://seasr.org/components/")
 
 public class EvernoteWriter implements ExecutableComponent {
-    @ComponentInput(description="Content to be written to Evernote."+
+    @ComponentInput(description="String to be written to Evernote."+
             "<br>TYPE: java.lang.String",
-                    name= "Content")
-    public final static String DATA_INPUT = "Content";
-    
+                    name= "string")
+    public final static String DATA_INPUT = "string";
+
     @ComponentProperty(defaultValue="",
                        description="This property sets username.",
                        name="username")
@@ -85,15 +89,16 @@ public class EvernoteWriter implements ExecutableComponent {
                        name="password")
     final static String DATA_PROPERTY_2 = "password";
     @ComponentProperty(defaultValue="seasr-fd4932c2",
-                       description="This property sets API key.",
+                       description="This property sets API key. " +
+                       "The default key is for SEASR project only.",
                        name="key")
     final static String DATA_PROPERTY_3 = "key";
-    
+
     @ComponentProperty(defaultValue="seasr",
-                       description="This property sets title of input content.",
+                       description="This property sets title of input string.",
                        name="title")
     final static String DATA_PROPERTY_4 = "title";
-    
+
     /** When ready for execution.
     *
     * @param cc The component context
@@ -103,56 +108,59 @@ public class EvernoteWriter implements ExecutableComponent {
     public void execute(ComponentContext cc) throws ComponentExecutionException,
         ComponentContextException {
         String username = cc.getProperty(DATA_PROPERTY_1),
-               password = cc.getProperty(DATA_PROPERTY_2),  
+               password = cc.getProperty(DATA_PROPERTY_2),
                key = cc.getProperty(DATA_PROPERTY_3);
-        
+
         String title = cc.getProperty(DATA_PROPERTY_4);
-        
+
         String inputContent = (String)cc.getDataComponentFromInput(DATA_INPUT);
-        
+
         String userStoreUrl = "https://lb.evernote.com/edam/user";
         String noteStoreUrlBase = "http://lb.evernote.com/edam/note/";
-        
+
         try {
             THttpClient userStoreTrans = new THttpClient(userStoreUrl);
-            TBinaryProtocol userStoreProt = 
+            TBinaryProtocol userStoreProt =
                 new TBinaryProtocol(userStoreTrans);
-            
-            UserStore.Client userStore = 
+
+            UserStore.Client userStore =
                 new UserStore.Client(userStoreProt, userStoreProt);
             boolean versionOk = userStore.checkVersion("(Java)",
                     com.evernote.edam.userstore.Constants.EDAM_VERSION_MAJOR,
                     com.evernote.edam.userstore.Constants.EDAM_VERSION_MINOR);
+
             if (!versionOk) {
                 System.err.println("Incomatible EDAM client protocol version");
                 System.exit(1);
             }
-            AuthenticationResult authResult = 
-                userStore.authenticate(username, password, key);        
+
+            AuthenticationResult authResult =
+                userStore.authenticate(username, password, key);
             User user = authResult.getUser();
             String authToken = authResult.getAuthenticationToken();
-            System.out.println("Notes for " + user.getUsername() + ":");
+            System.out.println("Notes for " + user.getUsername());
             String noteStoreUrl = noteStoreUrlBase + user.getShardId();
-            THttpClient noteStoreTrans = 
+
+            THttpClient noteStoreTrans =
                 new THttpClient(noteStoreUrl);
-            TBinaryProtocol noteStoreProt = 
+            TBinaryProtocol noteStoreProt =
                 new TBinaryProtocol(noteStoreTrans);
-            NoteStore.Client noteStore = 
+            NoteStore.Client noteStore =
                 new NoteStore.Client(noteStoreProt, noteStoreProt);
 
-            /*String content = 
-                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + 
+            /*String content =
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
                 "<!DOCTYPE en-note SYSTEM \"http://xml.evernote.com/pub/enml.dtd\">\n" +
-                "<en-note>\n" + 
-                "<b><font size=\"5\">Here is a simple document:</font></b>\n" + 
-                "<br/>\n" + 
-                "<u>Things to work on:</u>\n" +
-                "<en-todo checked=\"true\"/> Write up ENML API documentation\n" + 
+                "<en-note>\n" +
+                "<b><font size=\"5\">Here is a simple document:</font></b>\n" +
                 "<br/>\n" +
-                "<en-todo/> Set up ENML API wiki\n" + 
-                "<br/>\n" + 
+                "<u>Things to work on:</u>\n" +
+                "<en-todo checked=\"true\"/> Write up ENML API documentation\n" +
+                "<br/>\n" +
+                "<en-todo/> Set up ENML API wiki\n" +
+                "<br/>\n" +
                 "</en-note>\n";*/
-            
+
             Note note = new Note();
             note.setContent(inputContent);
             note.setTitle(title);
@@ -161,13 +169,13 @@ public class EvernoteWriter implements ExecutableComponent {
             throw new ComponentExecutionException(e);
         }
     }
-    
+
     /**
      * Call at the end of an execution flow.
      */
     public void initialize(ComponentContextProperties ccp) {
     }
-    
+
     /**
      * Called when a flow is started.
      */
