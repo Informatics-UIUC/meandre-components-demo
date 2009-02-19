9f0ba5c73535be80dfd35606fe91ef94f6151322
Loretta Auvil <lauvil@illinois.edu>
2009-02-19 20:42:26 +0000

    new component that conditionally send the output to different outputs based on the format of the text

---
+++ src/org/meandre/components/text/transform/ProcessTextBasedOnFormat.java
@@ -0,0 +1,62 @@
+package org.meandre.components.text.transform;
+
+import org.meandre.annotations.Component;
+import org.meandre.annotations.ComponentInput;
+import org.meandre.annotations.ComponentOutput;
+import org.meandre.core.ComponentContext;
+import org.meandre.core.ComponentContextException;
+import org.meandre.core.ComponentContextProperties;
+import org.meandre.core.ComponentExecutionException;
+import org.meandre.core.ExecutableComponent;
+
+@Component(creator="Loretta Auvil",
+		description="This component extracts the text from a pdf document. "+
+		"The input is a String or URL specifiying the url of the pdf document. "+
+		"The output is the extracted text.",
+		name="ProcessTextBasedOnFormat",
+		tags="URL, text, pdf",
+		dependency={"FontBox-0.2.0-dev.jar"},
+		baseURL="meandre://seasr.org/components/")
+
+public class ProcessTextBasedOnFormat implements ExecutableComponent {
+
+	@ComponentInput(description="URL of the item." +
+			"<br>TYPE: java.io.String",
+			name="URL")                 
+			public final static String DATA_INPUT = "URL";
+	@ComponentOutput(description="URL of the pdf item." +
+			"<br>TYPE: java.io.String",
+			name="PDF_URL")                 
+			public final static String DATA_OUTPUT_PDF = "PDF_URL";
+	@ComponentOutput(description="URL of the html item." +
+			"<br>TYPE: java.io.String",
+			name="HTML_URL")                 
+			public final static String DATA_OUTPUT_HTML = "HTML_URL";
+	@ComponentOutput(description="URL of the text item." +
+			"<br>TYPE: java.io.String",
+			name="TXT_URL")                 
+			public final static String DATA_OUTPUT_TXT = "TXT_URL";
+
+	public void dispose(ComponentContextProperties ccp)
+	throws ComponentExecutionException, ComponentContextException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void execute(ComponentContext cc)
+	throws ComponentExecutionException, ComponentContextException {
+
+		String url = (String)cc.getDataComponentFromInput(DATA_INPUT);
+		if (url.endsWith(".pdf"))
+			cc.pushDataComponentToOutput(DATA_OUTPUT_PDF, url);
+		else if (url.endsWith(".html") || (url.endsWith(".xml")))
+			cc.pushDataComponentToOutput(DATA_OUTPUT_HTML, url);
+		else
+			cc.pushDataComponentToOutput(DATA_OUTPUT_TXT, url);
+	}
+
+	public void initialize(ComponentContextProperties ccp)
+	throws ComponentExecutionException, ComponentContextException {
+		// TODO Auto-generated method stub
+	}
+}
